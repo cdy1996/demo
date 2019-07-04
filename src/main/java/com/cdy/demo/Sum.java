bb3b73cde3eeefc650aeee3f79831b4852f2460a
cdy1996 <13867868445@163.com>
2019-07-04 23:23:49 +0800

    说明: 算法

--- src/main/java/com/cdy/demo/Sum.java
+++ src/main/java/com/cdy/demo/Sum.java
@@ -1,36 +1,40 @@
 package com.cdy.demo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 查找有序数组中是否存在一堆数字相加等于给定值
  */
 public class Sum {
-
+    
     /**
      * 查找有序数组中是否存在一堆数字相加等于给定值
      *
      * @param data 有序数组
      * @param sum  给定值
-     * @return  是否存在
+     * @return 是否存在
      */
     public static boolean find(int[] data, int sum) {
-        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> set = new HashMap<>();
         for (int datum : data) {
-            set.add(datum);
+            set.put(datum, set.getOrDefault(datum, 0) + 1);
         }
         for (int d : data) {
-            if (set.contains(sum - d)) {
-                return true;
+            Integer temp = set.getOrDefault(sum - d, 0);
+            if (temp >= 1) {
+                return sum - d != d || temp > 1;  //在差值和d相等时检查是否数组中有两个以上d
             }
         }
         return false;
     }
-
-
+    
+    
     public static void main(String[] args) {
         System.out.println(find(new int[]{1, 2, 3, 9}, 8));
-        System.out.println(find(new int[]{1, 2, 4, 5}, 8));
+        System.out.println(find(new int[]{1, 3, 4, 5}, 8));
+        System.out.println(find(new int[]{1, 1}, 2));
+        System.out.println(find(new int[]{1}, 1));
+        System.out.println(find(new int[]{}, 1));
     }
 }
--- src/main/java/com/cdy/demo/TopK.java
+++ src/main/java/com/cdy/demo/TopK.java
@@ -1,30 +1,55 @@
 package com.cdy.demo;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
+/**
+ * 取n个有序数组的最大前k个
+ */
 public class TopK {
-
-
-    public static long[] topn(long[][] array, int k) {
-        //大根堆
-        PriorityQueue<Long> queue = new PriorityQueue<>(k, Comparator.reverseOrder());
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                queue.add(array[i][j]);
-            }
+    
+    static class Node {
+        Long data;
+        int x; //第几个数组
+        int y; // 数组中的哪一位
+        public Node(Long data, int x, int y) {
+            this.data = data;
+            this.x = x;
+            this.y = y;
         }
-        long[] longs = new long[k];
-        for (int i = 0; i < longs.length; i++) {
-            if (queue.peek()!=null) {
-                longs[i] = queue.poll();
-            }
-        }
-        return longs;
     }
-
-
+    
+    public static long[] topn(long[][] array, int k) {
+        if ( k==0 ) return new long[0];
+        int total=0;
+        //大根堆
+        PriorityQueue<Node> queue = new PriorityQueue<>(k, (a, b) -> b.data.compareTo(a.data));
+        // 塞入每个数组最后一个
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i].length <=0) continue;
+            queue.add(new Node(array[i][array[i].length - 1], i, array[i].length - 1));
+            total+=array[i].length;
+        }
+        // 取出第一个,然后放入一个取出元素所属的数组的上一个元素
+        int i1 = k > total ? total : k;
+        long[] result = new long[i1];
+        for (int i = 0, j = 0; i < i1; i++) {
+            Node node = queue.poll();
+            if (node == null) continue;
+            int y = --node.y;
+            if (y >= 0) {
+                queue.add(new Node(array[node.x][y], node.x, y));
+            }
+            result[j++] = node.data;
+        }
+        return result;
+    }
+    
+    
     public static void main(String[] args) {
-//        topn(new long[][]{[],[],[]}, int k)
+        System.out.println(Arrays.toString(topn(new long[][]{{1, 2, 3}, {7, 8, 9}, {4, 5, 6}}, 5)));
+        System.out.println(Arrays.toString(topn(new long[][]{{1, 2, 3}, {7, 8, 9, 10}, {4, 5, 6}}, 5)));
+        System.out.println(Arrays.toString(topn(new long[][]{{}, {}, {}}, 5)));
+        System.out.println(Arrays.toString(topn(new long[][]{{}, {}, {}}, 0)));
     }
 }
