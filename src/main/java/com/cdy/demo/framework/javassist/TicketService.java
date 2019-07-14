80597727936d22a466e67649a83992a6861dbac2
cdy1996 <13867868445@163.com>
2019-07-14 11:48:57 +0800

    说明: javassist demo

---
+++ src/main/java/com/cdy/demo/framework/javassist/TicketService.java
@@ -0,0 +1,17 @@
+package com.cdy.demo.framework.javassist;
+
+/**
+ * 售票服务接口 
+ */
+public interface TicketService {
+ 
+	//售票
+	public void sellTicket();
+	
+	//问询
+	public void inquire();
+	
+	//退票
+	public void withdraw();
+	
+}
