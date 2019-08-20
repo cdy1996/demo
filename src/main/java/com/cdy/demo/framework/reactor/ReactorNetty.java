3a5e1343d87710b4f1f1b3ff1679bb962d47a6b0
cdy1996 <chendongyi1996@gmail.com>
2019-08-20 11:04:04 +0800

    说明: 异步信号量 但是感觉没啥意义

--- src/main/java/com/cdy/demo/framework/reactor/ReactorNetty.java
+++ src/main/java/com/cdy/demo/framework/reactor/ReactorNetty.java
@@ -1,8 +1,8 @@
 package com.cdy.demo.framework.reactor;
 
-import reactor.core.publisher.Mono;
-import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
+
+import java.io.IOException;
 
 /**
  * todo
@@ -12,11 +12,29 @@ import reactor.netty.http.server.HttpServer;
 public class ReactorNetty {
     
     
-    public static void main(String[] args) {
-        Mono<? extends DisposableServer> bind = HttpServer.create()
+    public static void main(String[] args) throws IOException {
+         HttpServer.create()
                 .host("127.0.0.1")
                 .port(8080)
-                .bind();
+                .route(r->
+                        r.get("/a", (request, response)->{
+                            System.out.println("ddd");
+                    return response.sendString(request.receive()
+                            .asString()
+                            .map(it -> {
+                                System.out.println(request.param("param"));
+                                return it + ' ' + request.param("param") + '!';
+                            })
+                            .doOnEach(System.out::println));
+                }))
+//                .handle((request, response)-> response.sendString(request.receive()
+//                        .asString()
+//                        .log("all")
+//                        .map(it -> it + ' ' + request.param("param") + '!')
+//                        .log("all")))
+                .bindNow();
+
+         System.in.read();
         
         
     }
---
+++ src/main/java/com/cdy/demo/repeatedWheels/myasyncsemaphore/AsyncSemaphone.java
@@ -0,0 +1,55 @@
+package com.cdy.demo.repeatedWheels.myasyncsemaphore;
+
+import java.util.concurrent.*;
+
+public class AsyncSemaphone implements Runnable {
+    private final ExecutorService semaphoneExecutor = Executors.newSingleThreadExecutor();
+    private final ExecutorService taskExecutor;
+
+    private final Semaphore semaphore;
+
+    public final BlockingQueue<AsyncSemaphoneTask<?>> queue;
+
+    public AsyncSemaphone(int num) {
+        this(num, Integer.MAX_VALUE);
+    }
+
+    public AsyncSemaphone(int num, int queueNum) {
+        this(num, queueNum, 5);
+    }
+
+    public AsyncSemaphone(int num, int queueNum, int threadNum) {
+        semaphore = new Semaphore(num);
+        queue = new ArrayBlockingQueue<>(queueNum);
+        taskExecutor = Executors.newFixedThreadPool(threadNum);
+        semaphoneExecutor.submit(this);
+    }
+
+    public void submit(AsyncSemaphoneTask<?> task) {
+        queue.add(task);
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                AsyncSemaphoneTask<?> take = queue.take();
+                semaphore.acquire();
+
+                taskExecutor.submit(() -> {
+                    try {
+                        take.call();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        semaphore.release();
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
