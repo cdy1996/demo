bab8b6520ed6891f255db50bb8f073213d49d632
cdy1996 <chendongyi1996@gmail.com>
2019-08-21 20:20:07 +0800

    说明: CompleteFuture demo进一步理解

--- src/main/java/com/cdy/demo/java/juc/CompleteFutureTest.java
+++ src/main/java/com/cdy/demo/java/juc/CompleteFutureTest.java
@@ -1,19 +1,75 @@
 package com.cdy.demo.java.juc;
 
+import io.netty.util.concurrent.CompleteFuture;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class CompleteFutureTest {
-    
-    
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Future<Double> priceAsync = getPriceAsync("!23");
-        System.out.println(priceAsync.get());
-        
+
+
+    /**
+     * Thread-0111-2    2,5是先后关系,因为是通过返回值再去监听的
+     * Thread-0111-5
+     * Thread-0111-1    2,1,3是倒序输出的,因为是针对同一个对象的监听,会被倒序执行
+     * Thread-0111-3    3,6也是先后关系,因为是通过返回值再去监听的
+     * Thread-0222-6
+     * Thread-1111   -4
+     * Thread-1333   -7
+     */
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+//        Future<Double> priceAsync = getPriceAsync("!23");
+//        System.out.println(priceAsync.get());
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        CompletableFuture<String> first = CompletableFuture.supplyAsync(() -> {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "111";
+        });
+
+
+        CompletableFuture<String> third = first.thenApply((r) -> {
+            System.out.println(Thread.currentThread().getName() + r + "-3");
+            return "222";
+        });
+        first.whenComplete((r,e)->{
+            System.out.println(Thread.currentThread().getName()+r+"-1");
+        });
+        // first.whenComplete -> first.whenComplete 这种时倒序输出的
+        CompletableFuture<String> second = first.whenComplete((r, e) -> {
+            System.out.println(Thread.currentThread().getName()+r + "-2");
+        });
+
+        // 这个永远在都输第二 是因为是异步的关系
+        CompletableFuture<String> fourth = first.thenApplyAsync(r -> {
+            System.out.println(Thread.currentThread().getName() + r + "   -4");
+            return "333";
+        });
+
+        // first -> second 这种时顺序输出的
+        second.whenComplete((r, e) -> {
+             System.out.println(Thread.currentThread().getName()+r + "-5");
+        });
+
+        third.whenComplete((r, e) -> {
+            System.out.println(Thread.currentThread().getName() + r + "-6");
+        });
+        fourth.whenComplete((r, e) -> {
+            System.out.println(Thread.currentThread().getName() + r + "   -7");
+        });
+
+        countDownLatch.countDown();
+        System.in.read();
+
     }
     
     public static Future<Double> getPriceAsync(String product) {
