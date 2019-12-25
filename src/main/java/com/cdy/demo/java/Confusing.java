48a8df676deeae47dc89686cb17327142ab592cc
cdy1996 <chendongyi1996@gmail.com>
2019-12-25 23:56:19 +0800

    说明:  泛型 的问题

--- src/main/java/com/cdy/demo/java/Confusing.java
+++ src/main/java/com/cdy/demo/java/Confusing.java
@@ -1,7 +1,7 @@
 package com.cdy.demo.java;
 
 public class Confusing {
-
+    
 
     public Confusing(Object o){
 
@@ -15,9 +15,38 @@ public class Confusing {
 //
 //    }
 
+    
+    
+    static class ClassOne{}
+    static class ClassTwo{}
+    static interface InterfaceOne{}
+    
+    static <T extends ClassOne> T getClassOne(){return null;}
+    static <T extends InterfaceOne> T getInterfaceOne(){
+//        return (T)new InterfaceOne(){};
+        return null;
+    }
+    
+    static <T> void printType(T o) {
+        System.out.println("genericPrint");
+    }
+   
+    static <T extends ClassTwo> void printType(T o) {
+        System.out.println("T is Class two");
+    }
+    
     public static void main(String[] args) {
-        new Confusing(null);
-        new Confusing(1);
+//        new Confusing(null);
+//        new Confusing(1);
+    
+    
+        ClassOne classOne = getClassOne();
+        InterfaceOne interfaceOne = getInterfaceOne();
+        printType(getClassOne());
+        printType(getInterfaceOne());
+        printType(classOne);
+        printType(interfaceOne);
     }
 
 }
+
