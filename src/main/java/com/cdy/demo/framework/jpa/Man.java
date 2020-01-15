f6c2909b91f04f990118899e1bb84e4c8b2a36ed
cdy1996 <chendongyi1996@gmail.com>
2020-01-15 14:18:38 +0800

    说明: 减少 依赖附带, 注释掉有用的依赖

--- src/main/java/com/cdy/demo/framework/jpa/Man.java
+++ src/main/java/com/cdy/demo/framework/jpa/Man.java
@@ -1,24 +1,24 @@
-package com.cdy.demo.framework.jpa;
-
-import lombok.Data;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="MAN")
-@Data
-public class Man {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    private boolean disabled;
-
-}
+//package com.cdy.demo.framework.jpa;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="MAN")
+//@Data
+//public class Man {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//
+//    @Column(nullable = false)
+//    private String name;
+//
+//    @Column(nullable = false, unique = true)
+//    private String email;
+//
+//    private boolean disabled;
+//
+//}
