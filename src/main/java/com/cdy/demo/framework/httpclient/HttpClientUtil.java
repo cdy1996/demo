5bbd74b61d283675ba0a246fb718fa73835bc370
cdy1996 <chendongyi1996@gmail.com>
2020-03-10 10:24:04 +0800

    说明: httpclient  工具类

--- src/main/java/com/cdy/demo/framework/httpclient/HttpClientUtil.java
+++ src/main/java/com/cdy/demo/framework/httpclient/HttpClientUtil.java
@@ -8,28 +8,71 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class HttpClientUtil {
+	static PoolingHttpClientConnectionManager connectionManager;
+	static HttpClient httpClient;
+
+	static{
+		try {
+		     // 配置同时支持 HTTP 和 HTPPS
+		     SSLContextBuilder builder = new SSLContextBuilder();
+		     builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+		     SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(builder.build());
+		     Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+					 .register("http", PlainConnectionSocketFactory.getSocketFactory())
+					 .register("https", sslConnectionSocketFactory).build();
+		     connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+		 }catch (Exception e){
+		     log.error("初始化http 连接池异常",e);
+		     connectionManager = new PoolingHttpClientConnectionManager();
+		 }
+		connectionManager.setMaxTotal(20);
+		connectionManager.setDefaultMaxPerRoute(100);
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setConnectTimeout(1000)
+				.setConnectionRequestTimeout(2000)
+				.setSocketTimeout(3000).build();
+		HttpRequestRetryHandler retryHandler = new StandardHttpRequestRetryHandler();
+		httpClient = HttpClientBuilder.create()
+				.setDefaultRequestConfig(requestConfig)
+				.setRetryHandler(retryHandler)
+				.setConnectionManager(connectionManager).build();
+	}
 
 	/**
 	 * @Title: doGet
 	 * @Description: get方式
-	 * @param url请求路径
-	 * @param params参数
+	 * @param url 请求路径
+	 * @param params 参数
 	 * @author Mundo
 	 */
 	public static String doGet(String url, Map<String, String> params) {
@@ -37,7 +80,6 @@ public class HttpClientUtil {
 		// 返回结果
 		String result = "";
 		// 创建HttpClient对象
-		HttpClient httpClient = HttpClientBuilder.create().build();
 		HttpGet httpGet = null;
 		try {
 			// 拼接参数,可以用URIBuilder,也可以直接拼接在？传值，拼在url后面，如下--httpGet = new
@@ -86,7 +128,6 @@ public class HttpClientUtil {
 	public static String doPost(String url, Map<String, String> params) {
 		String result = "";
 		// 创建httpclient对象
-		HttpClient httpClient = HttpClientBuilder.create().build();
 		HttpPost httpPost = new HttpPost(url);
 		try { // 参数键值对
 			if (null != params && !params.isEmpty()) {
@@ -131,7 +172,6 @@ public class HttpClientUtil {
 	public static String sendJsonStr(String url, String params) {
 		String result = "";
  
-		HttpClient httpClient = HttpClientBuilder.create().build();
 		HttpPost httpPost = new HttpPost(url);
 		try {
 			httpPost.addHeader("Content-type", "application/json; charset=utf-8");
